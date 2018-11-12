de323af551208962c7b4de825ce7296176c94d43
Alexander <nemechekalexander@gmail.com>
2018-11-12 14:03:30 -0700

    final todo

--- exercises/TODO LIST WITH AXIO/app.js
+++ exercises/TODO LIST WITH AXIO/app.js
@@ -43,19 +43,19 @@ function listTodos(arr){
         
          let editTitle = document.createElement('input')
              editTitle.id = "id"
-             editTitle.placeholder = "add new title:"
+             editTitle.placeholder = "title: " + arr[i].title
             
          let editPrice = document.createElement("input")
              editPrice.id = "id2"
-             editPrice.placeholder = "add new price:"
+             editPrice.placeholder = "price: " + arr[i].price
 
          let editDescription = document.createElement("input")
              editDescription.id = "id3"
-             editDescription.placeholder = "add new description:"
+             editDescription.placeholder = "description: " + arr[i].description
              
          let editImage = document.createElement("input")
              editImage.id = "id4"
-             editImage.placeholder = "add new image:"
+             editImage.placeholder = "add new url:"
              editImage.setAttribute('src', arr[i].imgUrl)
              editImage.setAttribute('width', '100px')
              editImage.setAttribute('height', '100px')
@@ -66,6 +66,13 @@ function listTodos(arr){
          
              editButton.addEventListener("click", function(){
                 if(editButton.textContent === "Edit"){
+                    todoContainer.removeChild(title)
+                    todoContainer.removeChild(price)
+                    todoContainer.removeChild(description)
+                    todoContainer.removeChild(checkbox)
+                    todoContainer.removeChild(complete1)
+                    todoContainer.removeChild(image)
+                    
                     todoContainer.appendChild(editTitle) 
                     todoContainer.appendChild(editPrice) 
                     todoContainer.appendChild(editDescription) 
@@ -84,6 +91,13 @@ function listTodos(arr){
                    todoContainer.removeChild(editPrice)
                    todoContainer.removeChild(editDescription)
                    todoContainer.removeChild(editImage)
+                   
+                   todoContainer.appendChild(title)
+                   todoContainer.appendChild(price)
+                   todoContainer.appendChild(description)
+                   todoContainer.appendChild(complete1)
+                   todoContainer.appendChild(checkbox)
+                   todoContainer.appendChild(image)
                    editButton.textContent = "Edit" 
                }  
             })
