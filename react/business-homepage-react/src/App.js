3930da37d739a180652cf0e54552f05f3a239b47
Alexander <nemechekalexander@gmail.com>
2018-11-25 20:43:02 -0700

    new

---
+++ react/business-homepage-react/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Info from './Info'
+import List from './List'
+import Header from './Header'
+import Footer from './Footer'
+
+const App = () => {
+    return(
+   <div >
+    <Navbar />
+    <Header />
+    <Info />
+    <List />
+    <Footer />
+   </div>
+    )
+    
+}
+
+
+export default App
---
+++ react/business-homepage-react/src/Info.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Info = () => {
+return(
+<div className = "info">
+<h1>Advice That Gives Your Business an Edge</h1>
+<p> Give your business an edge by partnering with Global Business Solutions, LLC. 
+    Our commercial advisors in Salt Lake City, UT provide accounting insight and financial advice to help you cut costs and avoid audits. 
+    We also help you plan for your business’s future, whether that means expanding to another location or undergoing a successful sale.
+</p>
+</div>
+
+
+
+
+)
+}
+
+  export default Info
