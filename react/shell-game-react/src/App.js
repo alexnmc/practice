f05cb45f83f5092a911fa73889af2dbb9a00354d
Alexander <nemechekalexander@gmail.com>
2018-12-13 13:12:05 -0700

    update

--- react/shell-game-react/src/App.js
+++ react/shell-game-react/src/App.js
@@ -36,104 +36,136 @@ class App extends Component {
                 color: ''
             },
 
-            score: 0
-            
-            
-        }
-    }
-            
-    function1 = () => {
-        if(Math.floor(Math.random()*(1 - 3) + 3) === 1){
-            this.setState( prevState => {
-                return {
-                    box1: { backgroundColor: "rgba(245, 245, 245, 0.055)" , text: '.'},
-                    box2: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
-                    box3: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
-                    h1: {text: 'YOU WON!' , color: "rgba(36, 26, 11, 0.877)"},
-                    h1wrapper: {backgroundColor: 'rgba(245, 245, 245, 0.055)' },
-                    score: prevState.score + 1
-                }
-            })
-        } else {
-            this.setState( prevState => {
-                return {
-                    box1: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
-                    box2: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
-                    box3: { backgroundColor: "rgba(245, 245, 245, 0.055)", text: '.'},
-                    h1: {text: 'LOOOSER!' , color: "rgba(36, 26, 11, 0.877)"},
-                    h1wrapper: {backgroundColor: 'rgba(245, 245, 245, 0.055)'},
-                    score: prevState.score -1
-                }
+            score: 0,
 
-            })
+            functionOn: false
+
+        }
+
+    }
+    
+    
+
+    function1 = () => {
+        if(this.state.functionOn === true){
+                this.setState({
+                    functionOn: false
+                })
+            if(Math.floor(Math.random()*(1 - 3) + 3) === 1){
+                this.setState({
+                    functionOn: true
+                })
+                this.setState( prevState => {
+                    return {
+                        box1: { backgroundColor: "rgba(245, 245, 245, 0.055)" , text: '.'},
+                        box2: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
+                        box3: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
+                        h1: {text: 'YOU WON!' , color: "rgba(36, 26, 11, 0.877)"},
+                        h1wrapper: {backgroundColor: 'rgba(245, 245, 245, 0.055)' },
+                        score: prevState.score + 1,
+                    }
+                })
+            } else {
+                this.setState( prevState => {
+                    return {
+                        box1: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
+                        box2: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
+                        box3: { backgroundColor: "rgba(245, 245, 245, 0.055)", text: '.'},
+                        h1: {text: 'LOOOSER!' , color: "rgba(36, 26, 11, 0.877)"},
+                        h1wrapper: {backgroundColor: 'rgba(245, 245, 245, 0.055)'},
+                        score: prevState.score -1,
+                        
+                    }
+                })
+            }    
         }
     }
 
     function2 = () => {
-        if(Math.floor(Math.random()*(1 - 3) + 3) === 1){
-            this.setState( prevState => {
-               return{
-                    box1: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
-                    box2: { backgroundColor: "rgba(245, 245, 245, 0.055)", text: '.' },
-                    box3: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
-                    h1: {text: 'YOU WON!' , color: "rgba(36, 26, 11, 0.877)"},
-                    h1wrapper: {backgroundColor: 'rgba(245, 245, 245, 0.055)'},
-                    score: prevState.score +1
-                }
+        if(this.state.functionOn === true){
+            this.setState({
+                functionOn: false
             })
-        } else {
-            this.setState( prevState => {
-                return{
-                    box1: { backgroundColor: "rgba(245, 245, 245, 0.055)", text: '.'},
-                    box2: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
-                    box3: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
-                    h1: {text: 'LOOOSER!' , color: "rgba(36, 26, 11, 0.877)"},
-                    h1wrapper: {backgroundColor: 'rgba(245, 245, 245, 0.055)'},
-                    score: prevState.score -1
-                }
+            if( Math.floor(Math.random()*(1 - 3) + 3) === 1){
+                this.setState( prevState => {
+                    return{
+                        box1: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
+                        box2: { backgroundColor: "rgba(245, 245, 245, 0.055)", text: '.' },
+                        box3: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
+                        h1: { text: 'YOU WON!' , color: "rgba(36, 26, 11, 0.877)"},
+                        h1wrapper: { backgroundColor: 'rgba(245, 245, 245, 0.055)'},
+                        score: prevState.score +1,
+                        
+                    }
+                })
+            } else {
+                this.setState( prevState => {
+                    return{
+                        box1: { backgroundColor: "rgba(245, 245, 245, 0.055)", text: '.'},
+                        box2: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
+                        box3: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
+                        h1: { text: 'LOOOSER!' , color: "rgba(36, 26, 11, 0.877)"},
+                        h1wrapper: { backgroundColor: 'rgba(245, 245, 245, 0.055)'},
+                        score: prevState.score -1,
+                    
+                    }
 
-            })
+                })
+            }
         }
+        
     }
 
     function3 = () => {
-        if(Math.floor(Math.random()*(1 - 3) + 3) === 1){
-            this.setState( prevState => {
-                return{
-                    box1: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
-                    box2: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
-                    box3: { backgroundColor: "rgba(245, 245, 245, 0.055)", text: '.'},
-                    h1: {text: 'YOU WON!' , color: "rgba(36, 26, 11, 0.877)"},
-                    h1wrapper: {backgroundColor: 'rgba(245, 245, 245, 0.055)'},
-                    score: prevState.score+1
-                }
+        if(this.state.functionOn === true){
+            this.setState({
+                functionOn: false
             })
-        } else {
-            this.setState(prevState => {
-                return{
-                    box1: { backgroundColor: "rgba(245, 245, 245, 0.055)", text: '.'},
-                    box2: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
-                    box3: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
-                    h1: {text: 'LOOOSER!' , color: "rgba(36, 26, 11, 0.877)"},
-                    h1wrapper: {backgroundColor: 'rgba(245, 245, 245, 0.055)'},
-                    score: prevState.score -1
-                }
-
-            })
-
-        }
+            if(Math.floor(Math.random()*(1 - 3) + 3) === 1){
+                this.setState( prevState => {
+                    return{
+                        box1: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
+                        box2: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
+                        box3: { backgroundColor: "rgba(245, 245, 245, 0.055)", text: '.'},
+                        h1: {text: 'YOU WON!' , color: "rgba(36, 26, 11, 0.877)"},
+                        h1wrapper: {backgroundColor: 'rgba(245, 245, 245, 0.055)'},
+                        score: prevState.score+1,
+                        
+                    }
+                })
+            } else {
+                this.setState(prevState => {
+                    return{
+                        box1: { backgroundColor: "rgba(245, 245, 245, 0.055)", text: '.'},
+                        box2: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
+                        box3: { backgroundColor: "rgba(245, 245, 245, 0.055)" },
+                        h1: {text: 'LOOOSER!' , color: "rgba(36, 26, 11, 0.877)"},
+                        h1wrapper: {backgroundColor: 'rgba(245, 245, 245, 0.055)'},
+                        score: prevState.score -1,
+                        
+                    }
+                })
+            }
+        }   
     }
 
     function4 = () => {
-            this.setState({
-                box1: { animation:"shake 1s , move 2.1s",  animationIterationCount: 'infinite, 2' },
-                box2: { animation:"shake 1s , move2 2.1s", animationIterationCount: 'infinite, 2' },
-                box3: { animation:"shake 1s , move3 2.1s", animationIterationCount: 'infinite, 2' },
-                h1:{text: 'CHOOSE ONE:', color: 'rgb(202, 143, 77)'},
-                h1wrapper: {backgroundColor: "rgba(36, 26, 11, 0.897)"}
-      
+        
+           this.setState({
+               functionOn: true
+           })
+            
+            this.setState(prevState => {
+                return{ 
+                    box1: { animation:"shake 1.2s , move 2.1s",  animationIterationCount: 'infinite, 3' },
+                    box2: { animation:"shake 1.1s , move2 2.1s", animationIterationCount: 'infinite, 3' },
+                    box3: { animation:"shake 1.2s , move3 2.1s", animationIterationCount: 'infinite, 3' },
+                    h1:{text: 'CHOOSE ONE:', color: 'rgb(202, 143, 77)'},
+                    h1wrapper: { backgroundColor: "rgba(38, 26, 11, 0.897)"},
+                }
             })
-
+        
+            
     }
 
     render(){
