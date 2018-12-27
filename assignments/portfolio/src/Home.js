3f903b317612daf3ad31fb3c31aae20a74ef3f23
Alexander <nemechekalexander@gmail.com>
2018-12-27 14:08:06 -0700

    portfolio

--- assignments/portfolio/src/Home.js
+++ assignments/portfolio/src/Home.js
@@ -3,22 +3,23 @@ import {Link} from 'react-router-dom'
 
 const Home = props => {
     return (
-        <div className="home"  onScrollCapture={props.changeColor}>
+        <div className="home"  onMouseMove={props.changeColor}>
        
         <div className = "home1"> 
-            <p className = 'navbarP2' style = {props.style.navbarP2} >Alexander Nemechek </p>
+            <p className = 'navbarP2' style = {props.style.navbarP2} > Alexander Nemechek </p>
+            <p className = "developer"> javascript developer</p>
         </div>
         <div className = "home2">
         
-            <p className = 'navbarP'></p>
-            <p  className = 'navbarP'>Welcome to my portofolio website</p>
+           
+            <p  className = 'navbarP'>Welcome to my portfolio website</p>
             <h5>this site was made with REACT</h5>
         
         <div className = "home3">
             <p className = "aboutP">
             My name is Alexander Nemechek, I am a full stack software developer.
             I enjoy being challenged and engaging with projects that require me to work outside my comfort and knowledge set, 
-            continuing to learn new languages and development techniques. I can provide the following:
+            continuing to learn new languages and development techniques. I can provide:
             </p>
        
         <div className = "provide"> 
