659f4ad13e781c9642e8a1e83765961bc8e91c0c
Alexander <nemechekalexander@gmail.com>
2018-12-27 21:36:49 -0700

    update

--- assignments/portfolio/src/Home.js
+++ assignments/portfolio/src/Home.js
@@ -3,17 +3,17 @@ import {Link} from 'react-router-dom'
 
 const Home = props => {
     return (
-        <div className="home"  onMouseMove={props.changeColor}>
+    <div className="home"  onMouseMove={props.changeColor}>
        
         <div className = "home1"> 
             <p className = 'navbarP2' style = {props.style.navbarP2} > Alexander Nemechek </p>
             <p className = "developer"> javascript developer</p>
         </div>
+       
         <div className = "home2">
-        
-           
-            <p  className = 'navbarP'>Welcome to my portfolio website</p>
+             <p  className = 'navbarP'>Welcome to my portfolio website</p>
             <h5>this site was made with REACT</h5>
+        </div>
         
         <div className = "home3">
             <p className = "aboutP">
@@ -22,42 +22,40 @@ const Home = props => {
             continuing to learn new languages and development techniques. I can provide:
             </p>
        
-        <div className = "provide"> 
-            <ul>
-            <li>Responsive HTML/CSS</li>
-            <li>JavaScript (vanilla, React)</li>
-            <li>High-level user experience</li>
-            <li>Best practices</li>
-            <li>Performance</li>
-            <li>Animation</li>
-            
-           </ul>
+            <div className = "provide"> 
+                    <ul>
+                        <li>Responsive HTML/CSS</li>
+                        <li>JavaScript (vanilla, React)</li>
+                        <li>High-level user experience</li>
+                        <li>Best practices</li>
+                        <li>Performance</li>
+                        <li>Animation</li>
+                        
+                    </ul>
 
-           <ul>
-            <li>Node.js</li>
-            <li>Npm</li>
-            <li>API integration and development with express</li>
-            <li>Mongoose</li>
-            <li>Database arhitecture in MongoDB</li>
-            <li>Git</li>
-            
-           </ul>
-        </div>
-        
-        <div className = 'inline'>
-        <Link className = 'linkedin1' to ={ "/https://www.linkedin.com/in/alexander-nemechek-10240a167"}>
-        <div className = "linkedin"></div>
-        </Link>
-        <Link className = 'git1' to ={ "/https://www.linkedin.com/in/alexander-nemechek-10240a167"}>
-        <div className = "git"></div>
-        </Link>
-        </div>
-        
-        </div>
-        
-        </div>
+                <ul>
+                    <li>Node.js</li>
+                    <li>Npm</li>
+                    <li>API integration and development with express</li>
+                    <li>Mongoose</li>
+                    <li>Database arhitecture in MongoDB</li>
+                    <li>Git</li>
+                    
+                </ul>
+            </div>
+                
+            <div className = 'inline'>
+                <Link className = 'linkedin1' to ={ "/https://www.linkedin.com/in/alexander-nemechek-10240a167"}>
+                <div className = "linkedin"></div>
+                </Link>
+                <Link className = 'git1' to ={ "/https://www.linkedin.com/in/alexander-nemechek-10240a167"}>
+                <div className = "git"></div>
+                </Link>
+            </div>
             
         </div>
+        
+    </div>
     )
 }
 
