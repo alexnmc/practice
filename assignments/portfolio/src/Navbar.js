50f9878904cb5df7de6d3e951c4974a45b8177e8
Alexander <nemechekalexander@gmail.com>
2019-01-02 21:10:18 -0700

    update

--- assignments/portfolio/src/Navbar.js
+++ assignments/portfolio/src/Navbar.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <div className="navbar">
-            <p className = 'navbarP2'>Alexander Nemechek</p>
+            
             <Link className = 'link' to="/">home</Link>
-            <Link className = 'link' to="/about">about me</Link>
+            <Link className = 'link' to="/about">about</Link>
             <Link className = 'link' to="/projects">projects</Link>
             <Link className = 'link' to="/contact">contact</Link>
         </div>
--- assignments/portfolio/src/Projects.js
+++ assignments/portfolio/src/Projects.js
@@ -6,16 +6,30 @@ import shell from  './Photos/shellgame.png'
 
 const Projects = () => {
     return (
-        <div>
+        <div className="block">
         <div className = "projects1">
             
             <p className = "work">My Work:</p>
         
         <div className="projects">
+            
+            <div className = 'excDiv'>
+                <a>
+                <img className ="exchange" alt = '' src = {exchange}/>
+                </a>
+                <p className = 'exchangeText'>React website displaying and converting all the foreign currencies including Bitcoin. The aplication gets the data from the Fixer API using Euro as base currency.</p>
+            </div>
 
-            <img className ="exchange" alt = '' src = {exchange}/>
-            <img className = "shell" alt = '' src = {shell}/>
-            <div className = "group"></div>
+            <div className = ' shellDiv'>
+                <a>
+                <img className = "shell" alt = '' src = {shell}/>
+                </a>
+                <p className = 'shellText'>Shell Game, a fullstack aplication  created in React with CSS animations and lots of JavaScript. Custom API routes built with Mongoose and Express to save player info in MongoDB.
+                  </p>
+            </div>
+            
+            
+            {/* <div className = "group"></div> */}
 
         </div>
        
