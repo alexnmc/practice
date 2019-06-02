6994af0f65929874e8cefde35268bb92c5150128
Alexander <nemechekalexander@gmail.com>
2019-06-02 14:46:35 +0300

    update

--- functions/sortArrayinSubbArray.js
+++ functions/sortArrayinSubbArray.js
@@ -1,24 +1,22 @@
 
 
 function sort(arr){
-    let arr2 = []
-    let arr3 = []
-  
-    for(let i = 0; i < arr.length; i++){
-       if(!arr2.includes(arr[i])){
-         arr2.push(arr[i])
-         arr3.push([])
-       }
+  let arr2 = []
+  let arr3 = []
+  for(let i = 0; i < arr.length; i++){ // pushes all elements in an array once
+     if(!arr2.includes(arr[i])){
+        arr2.push(arr[i])
+        arr3.push([])
+      }
+    for(let j = 0; j<arr2.length; j++){ // checks is there re any other elements mathching the one in the new arr and pushes all of them in a 3rd array
+       if(arr2[j] === arr[i]){
+       arr3[j].push(arr[i])
+      }
     }
-  
-    for(let j = 0; j < arr2.length; j++){
-       for(let x = 0; x < arr.length; x++){
-         if(arr2[j] === arr[x]){
-           arr3[j].push(arr[x])
-         }
-       }
-    }
-      return arr3
   }
+   return arr3
+}
+
+  
   
   sort([4,1,1,2,2,3,3,1,2,3])  // this function takes in an array of numbers and returns a new array with subarrays containing the same values in the same array
