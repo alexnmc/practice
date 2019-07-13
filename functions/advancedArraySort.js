9f659990cc1a7064ff44213f53d6d183ccd463b4
Alexander <nemechekalexander@gmail.com>
2019-07-13 18:42:14 +0300

    update

--- functions/advancedArraySort.js
+++ functions/advancedArraySort.js
@@ -4,13 +4,13 @@ function sort(arr){
   let arr2 = []
   let arr3 = []
   for(let i = 0; i < arr.length; i++){ // pushes all elements in an array once
-     if(!arr2.includes(arr[i])){
+      if(!arr2.includes(arr[i])){
         arr2.push(arr[i])
         arr3.push([])
       }
     for(let j = 0; j<arr2.length; j++){ // checks if there are any other elements mathching the one in the new arr and pushes all of them in a 3rd array
-       if(arr[i] === arr2[j]){
-       arr3[j].push(arr[i])
+      if(arr[i] === arr2[j]){
+        arr3[j].push(arr[i])
       }
     }
   }
