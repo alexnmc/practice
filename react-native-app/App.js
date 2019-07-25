68c843f8214da52d63ac15cd781b0a5b27eeab2c
Alexander <nemechekalexander@gmail.com>
2019-07-25 17:52:52 +0300

    react-native

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,25 +1,80 @@
-import React from 'react';
-import {ImageBackground, StyleSheet, Text, View } from 'react-native';
+import React, { Component }  from 'react';
+import {ScrollView, ImageBackground, StyleSheet, Text, View , Image} from 'react-native'
+import A from 'react-native-a'
+import axios from 'axios'
 
-export default function App() {
-  return (
-    <View style = {styles.body}> 
-      <ImageBackground source={require('./Photos/news.jpg')} 
-        style={{flex:1,  
-                position: 'absolute',
-                width: '10%',
-                height: '10%',
-                justifyContent: 'center',   
-                resizeMode: "repeat"}}>
-        <Text>News For You</Text>
-      </ImageBackground>
-    </View>
-  );
+ export default class App extends Component{
+  
+  constructor(){
+    super()
+    this.state = {
+        
+        articles:[],
+       
+        
+    }
+}
+  
+componentDidMount(){ 
+  axios.get( `https://newsapi.org/v2/top-headlines?country=au&apiKey=f64c9be83f094f43a2c3954a6c1ec8aa`)
+   .then(response => {
+      this.setState({
+          articles: response.data.articles,
+          
+      })
+  })
+}
+  
+  
+  
+render(){
+  
+  
+  const article = this.state.articles.map(item => {
+      return (
+          <View key={Math.random()} style = {styles.body}>
+              <Text > {item.title} </Text>
+              <Image source ={{uri: item.urlToImage}} style={{width: '100%', height: '50%', margin:'auto'}}/>
+              <Text> {item.description} </Text>
+              <Text >{item.source.name} </Text>
+              <View >
+                  <A href={item.url}>read more</A>
+              </View>
+          </View>
+      )
+  })
+  
+    return (
+      
+      <ScrollView style = {styles.body2}> 
+        <ImageBackground source={require('./Photos/news.jpg')} 
+          style={{flex:1,  
+                  position: 'fixed',
+                  width: '100%',
+                  height: '100%',
+                  justifyContent: 'center',   
+                  resizeMode: "repeat"}}>
+          </ImageBackground>
+          {article}
+      </ScrollView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
- body:{
-   width: '100%',
-   height: '100%'
- }
-});
+  body:{
+    width: '90%',
+    height: 300,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+   
+ },
+
+ body2:{
+  flex: 1,
+  width: '100%',
+  height: '100%',
+  
+}
+})
