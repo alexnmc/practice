3e7ea29b65931fc0f58054817fae46da10294346
Alexander <nemechekalexander@gmail.com>
2019-07-26 13:07:31 +0300

    more channels and styles

--- react-native-app/Button1.js
+++ react-native-app/Button1.js
@@ -8,8 +8,6 @@ import {StyleSheet, ScrollView, TouchableOpacity, Text, View , Button} from 'rea
         
         function getNews(url, name) {
            props.getNewsCountry(url, name)
-           props.editToggle()
-           
         }
     
        
@@ -23,42 +21,68 @@ import {StyleSheet, ScrollView, TouchableOpacity, Text, View , Button} from 'rea
                 accessibilityLabel="Learn more about this purple button"
             />
             {props.toggle ?
-                    <ScrollView style = {styles.navbarWrap}>
-                        <Button
-                                onPress={() => props.editToggle()}
-                                title="Argentina"
-                                color="tomato"
-                                accessibilityLabel="Learn more about this purple button"
-                        />
+                   <ScrollView style = {styles.navbarWrap}>
+                   <TouchableOpacity onPress={() => getNews('ar', 'Argentina')}>
+                       <Text style = {styles.newsLink}>Argentina</Text>
+                   </TouchableOpacity>
+               
+                   <TouchableOpacity onPress={() => getNews('au','Austrialia')}>
+                       <Text style = {styles.newsLink}>Australia</Text>
+                   </TouchableOpacity>
 
+                   <TouchableOpacity onPress={() => getNews('at','Austria')}>
+                       <Text style = {styles.newsLink}>Austria</Text>
+                   </TouchableOpacity>
 
+                   <TouchableOpacity onPress={() => getNews('be','Belgium')}>
+                       <Text style = {styles.newsLink}>Belgium</Text>
+                   </TouchableOpacity>
 
-                        <TouchableOpacity onPress={() => props.editToggle()}>
-                            <Text style = {styles.newsLink}>Argentina</Text>
-                        </TouchableOpacity>
-                    
-                        <TouchableOpacity onPress={() => getNews('au','Austrialia')}>
-                            <Text style = {styles.newsLink}>Australia</Text>
-                        </TouchableOpacity>
+                   <TouchableOpacity onPress={() => getNews('br','Brazil')}>
+                       <Text style = {styles.newsLink}>Brazil</Text>
+                   </TouchableOpacity>
 
-                        <TouchableOpacity onPress={() => getNews('at','Austria')}>
-                            <Text style = {styles.newsLink}>Austria</Text>
-                        </TouchableOpacity>
+                   <TouchableOpacity onPress={() => getNews("bg", "Bulgaria")}>
+                       <Text style = {styles.newsLink}>Bulgaria</Text>
+                   </TouchableOpacity>
 
-                        <TouchableOpacity onPress={() => getNews('be','Belgium')}>
-                            <Text style = {styles.newsLink}>Belgium</Text>
-                        </TouchableOpacity>
+                   <TouchableOpacity onPress={() => getNews('ca','Canada')}>
+                       <Text style = {styles.newsLink}>Canada</Text>
+                   </TouchableOpacity>
 
-                        <TouchableOpacity onPress={() => getNews('br','Brazil')}>
-                            <Text style = {styles.newsLink}>Brazil</Text>
-                        </TouchableOpacity>
+                   <TouchableOpacity onPress={() => getNews("cu", "Cuba")}>
+                       <Text style = {styles.newsLink}>Cuba</Text>
+                   </TouchableOpacity>
 
-                        <TouchableOpacity onPress={() => getNews('ca','Canada')}>
-                            <Text style = {styles.newsLink}>Canada</Text>
-                        </TouchableOpacity>
-                    </ScrollView>
+                   <TouchableOpacity onPress={() => getNews("co", "Colombia")}>
+                       <Text style = {styles.newsLink}>Colombia</Text>
+                   </TouchableOpacity>
+
+                   <TouchableOpacity onPress={() => getNews('cn','China')}>
+                       <Text style = {styles.newsLink}>China</Text>
+                   </TouchableOpacity>
+
+                   <TouchableOpacity onPress={() => getNews("cz", "Czech Republic")}>
+                       <Text style = {styles.newsLink}>Czech Republic</Text>
+                   </TouchableOpacity>
+               
+                   <TouchableOpacity onPress={() => getNews("eg", "Egypt")}>
+                       <Text style = {styles.newsLink}>Egypt</Text>
+                   </TouchableOpacity>
+
+                   <TouchableOpacity onPress={() => getNews("fr", "France")}>
+                       <Text style = {styles.newsLink}>Francce</Text>
+                   </TouchableOpacity>
+
+                   <TouchableOpacity onPress={() => getNews('de','Germany')}>
+                       <Text style = {styles.newsLink}>Germany</Text>
+                   </TouchableOpacity>
+                
+                </ScrollView>
+                
                 :
-                null
+                
+                 null
             }
             </View>
         )
@@ -74,6 +98,8 @@ import {StyleSheet, ScrollView, TouchableOpacity, Text, View , Button} from 'rea
 
         navbarWrap: {
             marginTop: 80, 
+            height:'100%' , 
+            width: '50%', 
             borderWidth: 0.5,
             borderColor: 'black',
         }
--- react-native-app/Home.js
+++ react-native-app/Home.js
@@ -29,10 +29,10 @@ render(){
       this.props.articles.map(item => {
       return (
           <View key={Math.random()} style = {styles.news}>
-              <Text style = {{margin:'auto'}}> {item.title} </Text>
+              <Text style = {styles.itemTitle}> {item.title} </Text>
               <Image source ={{uri: item.urlToImage}} style={{flex:1, width: '100%', height: '50%', margin:'auto'}}/>
-              <Text style = {{margin:'auto'}}> {item.description} </Text>
-              <Text style = {{margin:'auto'}}>{item.source.name} </Text>
+              <Text style = {styles.mainText}> {item.description} </Text>
+              <Text style = {styles.itemSource}>{item.source.name} </Text>
               <A href={item.url} style = {{margin:'auto'}}>read more</A>
           </View>
       )
@@ -52,23 +52,41 @@ render(){
 }
 
 const styles = StyleSheet.create({
-  news:{
-    width: '90%',
-    height: 300,
+    news:{
+      width: '90%',
+      height: 300,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop:50,
+    },
+
+  body2:{
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
-    marginTop:50,
-   
-   
- },
+    justifyContent: 'center'
+  },
 
- body2:{
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center'
-  
-}
+  itemTitle:{
+    color:'rgb(131, 0, 0)',
+    marginBottom: 3,
+    fontSize: 15,
+    fontWeight: "900",
+    width:'100%'
+  },
+
+  mainText:{
+    fontSize:14,
+    fontFamily: 'Arial',
+    marginTop: 3,
+    fontWeight: '900'
+  },
+
+  itemSource:{
+    color: 'rgb(128, 0, 0)'
+  }
+
+
 })
 
 export default withButton(App)
