2adb3da2247d250f499300375efefa8367ddb32a
Alexander <nemechekalexander@gmail.com>
2019-07-27 15:57:22 +0300

    new project

--- farma-up/client/src/Navbar.js
+++ farma-up/client/src/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Navbar = (props) => {
     return (
         <div className="navbar">
-            <Link to="/">Home</Link>
+            <Link to="/">Utilizator</Link>
             <Link to="/contact">Contact</Link>
             <Link to = "/pharma">Farmacie</Link>
         </div>
