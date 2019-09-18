78f5612312e1c8e89d9fbe6d5af3f2530dea9e47
Alexander <nemechekalexander@gmail.com>
2019-09-18 23:21:24 +0300

    update

--- react/business-homepage-react/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './style.css' // you can link it to App.js too if you want...
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'))
