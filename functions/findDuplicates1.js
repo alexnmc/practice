43ca4b0cf83b88acf141481af4bcd842234242e3
alex <nemechekalexander@gmail.com>
2020-01-26 16:48:01 -0600

    new function

--- functions/findDuplicates1.js
+++ functions/findDuplicates1.js
@@ -5,7 +5,7 @@ function find(str){
     let arr = []
     let input = str.split('')
     input.forEach(function(item,i){
-    if(input.indexOf(item)!= i && !arr.includes(item)){//indexOf(item) is the first index of the item in the array if the item exists under other index then it is a duplicate
+    if(input.indexOf(item)!== i && !arr.includes(item)){//indexOf(item) is the first index of the item in the array if the item exists under other index then it is a duplicate
        arr.push(item)
     }
   })
