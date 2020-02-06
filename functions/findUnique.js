8fab84a240476d88b4bdea3f617ab592d62b4307
alex <nemechekalexander@gmail.com>
2020-02-06 12:47:41 -0600

    update

--- functions/findUnique.js
+++ functions/findUnique.js
@@ -1,31 +1,21 @@
 
 
-
 function findUnique(arr){
     let arr2 = []
     let arr3 = []
-   
-    for(let i = 0; i<arr.length; i++){
-      for(let j = i+1; j<arr.length; j++){ // second loop starts onde index from i so it doesn't check against itself
-        if(arr[i] === arr[j] && !arr2.includes(arr[i])){
-          arr2.push(arr[i])
-            if(!arr2.includes(arr[i])){
-                arr3.push(arr[i])
-            }
+    arr.forEach((item,i) => {
+        if(arr.indexOf(item) !== i && !arr2.includes(item)){                //indexOf(item) is the first index of the item in the array if the item exists under other index then it is a duplicate
+            arr2.push(item)
         }
-       }
-    }
-
-    for(let y = 0; y<arr.length; y++){
-        if(!arr2.includes(arr[y])){
-            arr3.push(arr[y])
+    })
+    
+    arr.forEach(item => {
+        if(!arr2.includes(item)){
+            arr3.push(item)
         }
-    }
-    
+    })
      
-     
-    
-    return arr3
+       return arr3
   }
   
   console.log(findUnique([1,2,3,3,4,4,5,5,6]))
